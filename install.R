d75fc4d9ad64237402e094ba6a2866cfb9c4c5ed
zequihg50 <ezequiel.cimadevilla@unican.es>
2026-06-18 12:18:14 +0200

    add

---
+++ install.R
@@ -0,0 +1,6 @@
+
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools", repos = "https://cloud.r-project.org")
+}
+
+devtools::install_github("pacificclimate/climdex.pcic")
